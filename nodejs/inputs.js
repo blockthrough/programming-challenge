25e3f28b4e0818eea09fb0e4b71f0f31e0e55a59
andrew-jung <unkofunko@gmail.com>
2017-10-15 20:41:16 -0300

    Commit

--- nodejs/inputs.js
+++ nodejs/inputs.js
@@ -10,5 +10,46 @@ module.exports = {
       peanutButter: 10,
       jam: 5
     }
+  },
+  twoServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 10
+    }
+  },
+  missingIngredient: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10
+    }
+  },
+  noneFound: {
+  },
+  party: {
+    recipe: {
+      bread: 2,
+      peanutButter: 1,
+      jam: 1,
+      butter: 3,
+      mayo: 5
+    },
+    inventory: {
+      bread: 1231,
+      peanutButter: 4567,
+      jam: 8890,
+      butter: 1000,
+      mayo: 1000
+    }
   }
 }
--- nodejs/test.js
+++ nodejs/test.js
@@ -10,3 +10,39 @@ describe('Nomnomthrough', () => {
     });
   });
 });
+
+describe('Nomnomthrough', () => {
+  describe('When there is enough ingredient for two serving', () => {
+    it('should return 2', () => {
+      const output = howManyServings(inputs.twoServings);
+      assert.equal(output, 2);
+    });
+  });
+});
+
+describe('Nomnomthrough', () => {
+  describe('When there is a missing ingredient in inventory.', () => {
+    it('should return 0', () => {
+      const output = howManyServings(inputs.missingIngredient);
+      assert.equal(output, 0);
+    });
+  });
+});
+
+describe('Nomnomthrough', () => {
+  describe('When there is no elements', () => {
+    it('should return 0', () => {
+      const output = howManyServings(inputs.noneFound);
+      assert.equal(output, 0);
+    });
+  });
+});
+
+describe('Nomnomthrough', () => {
+  describe('When there is no elements', () => {
+    it('should return 0', () => {
+      const output = howManyServings(inputs.noneFound);
+      assert.equal(output, 0);
+    });
+  });
+});
