bf4ea945ca1a997acf8783eb465749462d1ed99e
Visaahan Anandarajah <visaahan@hotmail.com>
2017-10-27 14:57:13 -0400

    Initial commit of programming challenge

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,44 @@
 function howManyServings (params) {
-  const {recipe, inventory} = params
+    const {recipe, inventory} = params
 
-  // Write code to solve the problem here.
+    if (!params || !recipe || !inventory){
+        return 0;
+    }
+    
+    var servingCount = Number.MAX_VALUE;
 
-  return 0
+    for (ingredient in recipe){
+
+        // if ingredient not in inventory
+        if (!(ingredient in inventory)){
+            servingCount = 0;
+            break;
+        }
+
+        var inventoryRequired = recipe[ingredient];
+        var inventoryRemaining = inventory[ingredient];
+
+        //if negative inventory or zero required - not feasible
+        if (inventoryRequired <= 0){
+            servingCount = 0;
+            break;
+        }
+
+        // Note: Divide by 0 results in infinity
+        var servingsCreated = Math.floor(inventoryRemaining/inventoryRequired);
+
+        //if negative servingsCreated - not feasible
+        if (servingsCreated < 0){
+            servingCount = 0;
+            break;
+        }
+        //if you can create less servings that what you have counted, update the count
+        if (servingsCreated < servingCount){
+            servingCount = servingsCreated;
+        }
+    }
+
+    return servingCount;  
 }
 
 module.exports = {howManyServings}
--- nodejs/inputs.js
+++ nodejs/inputs.js
@@ -10,5 +10,102 @@ module.exports = {
       peanutButter: 10,
       jam: 5
     }
-  }
+  },
+  noServing: {
+    recipe: {
+      bread: 5,
+      jam: 5
+    },
+    inventory:{
+      bread:2,
+      jam: 6
+    }
+  },
+  ingredientDoesntExist: {
+    recipe: {
+      bread: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 5,
+      peanutButter: 5
+    }
+  },
+  multipleServings: {
+    recipe: {
+      bread: 2,
+      jam :2,
+    },
+    inventory: {
+      bread:8,
+      jam: 10
+    }
+  },
+  specialCharacters: {
+    recipe: {
+      "!@#":1,
+      "%^&":2
+    },
+    inventory: {
+      "!@#":1,
+      "%^&":2
+    }
+  },
+  negativeInventory:{
+    recipe: {
+      bread: 2,
+      jam :2,
+    },
+    inventory: {
+      bread:-2,
+      jam: -2
+    }
+  },
+  negativeRecipe:{
+    recipe: {
+      bread: -2,
+      jam :-2,
+    },
+    inventory: {
+      bread:2,
+      jam: 2
+    }
+  },
+  negativeRecipeAndInventory:{
+    recipe: {
+      bread: -2,
+      jam :-2,
+    },
+    inventory: {
+      bread:-2,
+      jam:-2
+    }
+  },
+  maxInteger:{
+    recipe: {
+      bread: Number.MAX_VALUE/2
+    },
+    inventory:{
+      bread: Number.MAX_VALUE
+    }
+  },
+  zeroRecipe:{
+    recipe:{
+      bread:0,
+    },
+    inventory:{
+      bread:1,
+    }
+  },
+  noRecipe:{
+    inventory: {
+      bread: 1
+    }
+  },
+  noIngredients:{
+    recipe:{
+      bread:1
+    }
+  },
+  noRecipeOrIngredients:{}
 }
