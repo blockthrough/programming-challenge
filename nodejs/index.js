7f3b2cc7b694c2841391334ab6acc73d16ea2c13
Cole Boudreau <cboudreau@knote.me>
2017-12-05 20:58:34 -0500

    DEV: Implemented and added tests

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,15 @@
 function howManyServings (params) {
   const {recipe, inventory} = params
 
-  // Write code to solve the problem here.
-
-  return 0
+  var currentMin = Number.POSITIVE_INFINITY;
+  for(var key in recipe) {
+    if (recipe[key] >= 0 && key in inventory && inventory[key] >= 0) {
+      currentMin = Math.min(currentMin, inventory[key] / recipe[key]);
+    } else {
+      return 0;
+    }
+  }
+  return currentMin
 }
 
 module.exports = {howManyServings}
--- nodejs/inputs.js
+++ nodejs/inputs.js
@@ -10,5 +10,88 @@ module.exports = {
       peanutButter: 10,
       jam: 5
     }
-  }
+  },
+  twoServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 10
+    }
+  },
+  zeroServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 0,
+      peanutButter: 0,
+      jam: 0
+    }
+  },
+  nullServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: null,
+      peanutButter: null,
+      jam: null
+    }
+  },
+  nanServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 'a',
+      peanutButter: 'b',
+      jam: 'c'
+    }
+  },
+  negServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: -1,
+      peanutButter: -1,
+      jam: -1
+    }
+  },
+  missingServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 10,
+      peanutButter: 5
+    }
+  },
+  zeroRecipe: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 0
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 5
+    }
+  },
 }
