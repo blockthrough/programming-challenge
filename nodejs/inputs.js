3d5c144c62df51cd5511fc6276a43308b6a5daeb
Zhiyu Pu <georgepu45@gmail.com>
2018-03-16 13:36:20 +0800

    edit test.js, index.js, inputs.js

--- nodejs/inputs.js
+++ nodejs/inputs.js
@@ -1,14 +1,74 @@
 module.exports = {
-  oneServing: {
-    recipe: {
-      bread: 10,
-      peanutButter: 5,
-      jam: 5
+    zeroServing: {
+        recipe: {
+            bread: 10,
+            peanutButter: 5,
+            jam: 5
+        },
+        inventory: {
+            bread: 20,
+            peanutButter: 10,
+            jam: 0
+        }
     },
-    inventory: {
-      bread: 20,
-      peanutButter: 10,
-      jam: 5
+    oneServing: {
+        recipe: {
+            bread: 10,
+            peanutButter: 5,
+            jam: 5
+        },
+        inventory: {
+            bread: 20,
+            peanutButter: 10,
+            jam: 5
+        }
+    },
+    twoServings: {
+        recipe: {
+            bread: 10,
+            peanutButter: 5,
+            jam: 5
+        },
+        inventory: {
+            bread: 20,
+            peanutButter: 12,
+            jam: 10
+        }
+    },
+    threeServings: {
+        recipe: {
+            bread: 10,
+            peanutButter: 5,
+            jam: 5
+        },
+        inventory: {
+            bread: 45,
+            peanutButter: 15,
+            jam: 22
+        }
+    },
+    fourServings: {
+        recipe: {
+            bread: 10,
+            peanutButter: 5,
+            jam: 5
+        },
+        inventory: {
+            bread: 45,
+            peanutButter: 25,
+            jam: 22
+        }
+    },
+    fiveServings: {
+        recipe: {
+            bread: 10,
+            peanutButter: 5,
+            jam: 5
+        },
+        inventory: {
+            bread: 59,
+            peanutButter: 45,
+            jam: 45
+        }
     }
-  }
 }
