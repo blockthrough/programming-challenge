563a31047d0d18b3bd912261d183699967ee06d0
Charles <charlesdsmith25@gmail.com>
2018-03-18 01:19:18 -0400

    Second commit

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,26 @@
 function howManyServings (params) {
   const {recipe, inventory} = params
 
-  // Write code to solve the problem here.
+  // use for-in loop to verify the input
+  for (var key in params) {
+  	for (var keys in params[key]) {
+  		obj = params[key]
+  		if (typeof(obj[keys]) != 'number') {
+  			return new TypeError('Only integers can be used as values!')
+  		}
+  	}
+  	}
 
-  return 0
+  // Divide the amount in the inventory by the amount required for the recipe
+  // round it down because there can't be a fraction of an ingredient, we can only use 
+  // what is enough for the recipe
+
+  var bread_confirm = Math.floor(inventory.bread / recipe.bread)
+  var peanutButter_confirm = Math.floor(inventory.peanutButter / recipe.peanutButter)
+  var jam_confirm = Math.floor(inventory.jam / recipe.jam)
+
+  minimum_servings = Math.min(bread_confirm, peanutButter_confirm, jam_confirm)
+  return minimum_servings
 }
 
 module.exports = {howManyServings}
