ba376908d205e0a77cdd8ae9404aefffe34f325b
Charles <charlesdsmith25@gmail.com>
2018-03-18 01:20:10 -0400

    second commit

--- nodejs/inputs.js
+++ nodejs/inputs.js
@@ -10,5 +10,57 @@ module.exports = {
       peanutButter: 10,
       jam: 5
     }
-  }
+  },
+
+  twoServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 10
+    }
+  },
+
+  onehundredServings: {
+    recipe: {
+      bread: 1,
+      peanutButter: 1,
+      jam: 1
+    },
+    inventory: {
+      bread: 1050,
+      peanutButter: 1023,
+      jam: 1000
+    }
+  },
+
+  nonintegerValue: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 'sfsf'
+    }
+  },
+
+  numberString: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: '10'
+    }
+  },
 }
