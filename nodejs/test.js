6ceeb3557b0dba2bb3e125a6b9c23df71b58ac8a
Charles <charlesdsmith25@gmail.com>
2018-03-18 01:20:58 -0400

    second commit

--- nodejs/test.js
+++ nodejs/test.js
@@ -9,4 +9,28 @@ describe('Nomnomthrough', () => {
       assert.equal(output, 1);
     });
   });
+  describe('When there is enough for two servings', () => {
+  	it('should return 2', () => {
+  		const output = howManyServings(inputs.twoServings);
+  		assert.equal(output, 2);
+  	});
+  });
+  describe('When there is enough for one hundred servings', () => {
+  	it('should return 1000', () => {
+  		const output = howManyServings(inputs.onehundredServings);
+  		assert.equal(output, 1000);
+  	});
+  });
+  describe('When the amount is not an integer', () => {
+  	it('should return NaN', () => {
+  		const output = howManyServings(inputs.nonintegerValue);
+  		assert.ok(isNaN(output));
+  	});
+  });
+  describe('When the amount is a number in a string', () => {
+  	it('should throw a TypeError', () => {
+  		const output = howManyServings(inputs.numberString);
+  		assert.ok(output, 'Only integers can be used as values!')
+  	});
+  });
 });
