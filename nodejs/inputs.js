163fe6268ebe1f25e777205c768615724d54bd1f
Nisanthan <>
2018-03-23 16:15:54 -0400

    wrote howManyServings function + various inputs + unit tests

--- nodejs/inputs.js
+++ nodejs/inputs.js
@@ -10,5 +10,102 @@ module.exports = {
       peanutButter: 10,
       jam: 5
     }
+  },
+  twoServings: {
+    recipe: {
+      bread: 4,
+      peanutButter: 5,
+      jam: 1
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 5
+    }
+  },
+  negativeServing: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: -20,
+      peanutButter: 10,
+      jam: 5
+    }
+  },
+  zeroServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 0,
+      peanutButter: 10,
+      jam: 5
+    }
+  },
+  stringValueServing: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 'bread',
+      peanutButter: 10,
+      jam: 5
+    }
+  },
+  numberStringServing: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: '20',
+      peanutButter: 10,
+      jam: 5
+    }
+  },
+  incompleteValueServing: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      peanutButter: 10,
+      jam: 5
+    }
+  },
+  nullValueServing: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: null,
+      peanutButter: 10,
+      jam: 5
+    }
+  },
+  differentRecipeOneServing: {
+    recipe: {
+      "olive oil": 10,
+      pasta: 5,
+      tomato: 5,
+      pesto: 3
+    },
+    inventory: {
+      "olive oil": 20,
+      pasta: 10,
+      tomato: 5,
+      pesto: 3
+    }
   }
 }
--- nodejs/test.js
+++ nodejs/test.js
@@ -1,11 +1,74 @@
-const assert = require('assert');
-const {howManyServings} = require('./index');
-const inputs = require('./inputs')
+const assert = require("assert");
+const { howManyServings } = require("./index");
+const inputs = require("./inputs");
 
-describe('Nomnomthrough', () => {
-  describe('When there is enough ingredient for one serving', () => {
-    it('should return 1', () => {
+describe("Nomnomthrough", () => {
+  describe("When there is enough ingredients for one serving", () => {
+    it("should return 1", () => {
       const output = howManyServings(inputs.oneServing);
+      assert.equal(output, 1);
+    });
+  });
+  describe("When there is enough ingredients for two servings", () => {
+    it("should return 2", () => {
+      const output = howManyServings(inputs.twoServings);
+      assert.equal(output, 2);
+    });
+  });
+  describe("When the value for an ingredient is a negative integer", () => {
+    it("should return an error message", () => {
+      try {
+        const output = howManyServings(inputs.negativeServing);
+      } catch (err) {
+        assert.equal(err.message, "-20 is not a positive integer.");
+      }
+    });
+  });
+  describe("When there is enough ingredients for zero servings", () => {
+    it("should return 0", () => {
+      const output = howManyServings(inputs.zeroServings);
+      assert.equal(output, 0);
+    });
+  });
+  describe("When the value for an ingredient is a string value", () => {
+    it("should return an error message", () => {
+      try {
+        const output = howManyServings(inputs.stringValueServing);
+      } catch (err) {
+        assert.equal(err.message, "\"bread\" is not an integer number.");
+      }
+    });
+  });
+  describe("When the value for an ingredient is a string number", () => {
+    it("should return an error message", () => {
+      try {
+        const output = howManyServings(inputs.numberStringServing);
+      } catch (err) {
+        assert.equal(err.message, "\"20\" is not an integer number.");
+      }
+    });
+  });
+  describe("When an ingredient in inventory is missing", () => {
+    it("should return an error message", () => {
+      try {
+        const output = howManyServings(inputs.incompleteValueServing);
+      } catch (err) {
+        assert.equal(err.message, "There is a missing ingredient.");
+      }
+    });
+  });
+  describe("When the value for an ingredient is null", () => {
+    it("should return an error message", () => {
+      try {
+        const output = howManyServings(inputs.nullValueServing);
+      } catch (err) {
+        assert.equal(err.message, "\"null\" is not an integer number.");
+      }
+    });
+  });
+  describe("When there is a different recipe and enough ingredients for one serving", () => {
+    it("should return 1", () => {
+      const output = howManyServings(inputs.differentRecipeOneServing);
       assert.equal(output, 1);
     });
   });
