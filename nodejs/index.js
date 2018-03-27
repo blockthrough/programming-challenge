e4e4210fc99663f788c33dd89523dfacf508dcdb
Shahin Rahbari <Shahin@v1020-wn-118-249.campus-dynamic.uwaterloo.ca>
2018-03-26 20:55:56 -0400

    code completed

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,7 +3,31 @@ function howManyServings (params) {
 
   // Write code to solve the problem here.
 
-  return 0
+  //Validating inventory and recipe values
+  for (var i = 0; i < Object.keys(inventory).length;i++){
+
+  	if((inventory[Object.keys(inventory)[i]] == undefined) || (recipe[Object.keys(recipe)[i]] == undefined)){
+  		throw new Error("undefined type not accepted");
+  	}
+  	if((typeof inventory[Object.keys(inventory)[i]] !== "number") || (inventory[Object.keys(inventory)[i]] < 0)){
+  		throw new Error("Inventory only accepts positive integers");
+  	}
+  	if((typeof recipe[Object.keys(recipe)[i]] !== "number") || (recipe[Object.keys(recipe)[i]] < 0)){
+  		throw new Error("Recipe only accepts positive integers");
+  	}
+  	if(Object.keys(inventory).length !== Object.keys(recipe).length){
+  		throw new Error("Number of items in inventory and recipe don't match");
+  	}
+  	if(Object.keys(inventory)[i] !== Object.keys(recipe)[i]) {
+  		throw new Error("Items in inventory and recipe don't match");	
+  	}
+  }
+  var ingredients = []
+  for (var i = 0; i < Object.keys(inventory).length;i++){
+  	ingredients.push(Math.floor(inventory[Object.keys(inventory)[i]] / recipe[Object.keys(recipe)[i]]));
+  }
+
+  return Math.min(...ingredients);
 }
 
 module.exports = {howManyServings}
