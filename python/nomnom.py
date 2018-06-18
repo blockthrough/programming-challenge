ccbe75509638685d4bea13d3e860da344b317a80
Rashheh <rasheedaabbas@gmail.com>
2018-06-18 19:47:28 +0500

    code and some tests

--- python/nomnom.py
+++ python/nomnom.py
@@ -1,5 +1,17 @@
-dev how_many_servings():
-	"""
-
-	"""
-	pass
+def how_many_servings(recipe, inventory):
+	count = 0
+	if set(recipe).issubset(set(inventory)):
+		while True:
+			ran = 0
+			for key in recipe:
+				if recipe[key] <= inventory[key]:
+					ran+=1
+					inventory[key]-=recipe[key]
+				else:
+					break
+			if ran == len(recipe):
+				count+=1
+				ran=0
+			else:
+				break
+	return count
--- python/tests.py
+++ python/tests.py
@@ -1,1 +1,14 @@
-# Write unit tests here
+import unittest
+import nomnom
+
+
+
+class TestNomNom(unittest.TestCase):
+	def test_how_many_servings(self):
+		self.assertEqual(how_many_servings({"bread": 10, "peanutButter": 5, "jam": 5}, {"bread": 20, "peanutButter": 10, "jam": 5}), 1)
+		self.assertEqual(how_many_servings({"bread": 15, "peanutButter": 25, "jam": 25}, {"bread": 45, "peanutButter": 100, "jam": 100}), 3)
+		self.assertEqual(how_many_servings({"apple": 5, "orange": 15, "strawberry": 10}, {"apple": 20, "orange": 30, "strawberry": 50}), 2)
+
+
+if __name__ == '__main__':
+	unittest.main()
