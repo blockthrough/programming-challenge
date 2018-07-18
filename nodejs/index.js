e5c96a17897658ee4cdc0f906587031bf24695fa
Taylor Ramsay <tayramsay@gmail.com>
2018-07-18 17:17:39 -0400

    Blockthrough challenge completed.

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,24 @@
 function howManyServings (params) {
   const {recipe, inventory} = params
-
-  // Write code to solve the problem here.
-
-  return 0
+  let ingredientsThatIHaveInInventory = []
+  let ingredientsInRecipe = []
+  let servingPerIngredient = []
+  for(ingredient in recipe){
+    ingredientsInRecipe.push(ingredient)
+    for(item in inventory){
+      if(item === ingredient && inventory[item] >= 0){
+        ingredientsThatIHaveInInventory.push(item)
+        servingPerIngredient.push(inventory[item]/recipe[ingredient])
+      }
+    }
+  }
+  if(ingredientsThatIHaveInInventory.length === ingredientsInRecipe.length && ingredientsThatIHaveInInventory.length > 0){
+    let minServingPerIngredient = Math.min(...servingPerIngredient)
+    return Math.floor(minServingPerIngredient)
+  }
+  else {
+    return 0
+  }
 }
 
 module.exports = {howManyServings}
