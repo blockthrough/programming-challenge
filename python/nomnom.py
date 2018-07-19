97feb6bdae525ed68ce8ea6820f46e8ef637dc2a
piter0900 <piter0900@gmail.com>
2018-07-19 13:38:56 -0700

    Update nomnom.py

--- python/nomnom.py
+++ python/nomnom.py
@@ -1,5 +1,19 @@
-dev how_many_servings():
-	"""
+import math
 
-	"""
-	pass
+
+def how_many_servings(recipe, inventory):
+
+	max = float("inf")
+	for item in recipe:
+		try:
+			tmp = math.floor(inventory[item]/recipe[item]) 
+		except BaseException as e:
+			if e.args[0] != 'division by zero':
+			#if an item has value of 0 in recipe, do nothing
+				return "something is missing"
+				#else, something is missing from the inventory and we can't make any
+		if tmp<max:
+			max = tmp
+	if max == float("inf"):
+			return "something is missing"
+	return max
