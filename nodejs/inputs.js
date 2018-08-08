8fb20a978bf90199ab0a7059ee347d9e24cee2ba
Marco Andrade <marcoandrade@MacBook-Air-2.local>
2018-08-08 12:39:30 -0400

    Solution to coding challenge

--- nodejs/inputs.js
+++ nodejs/inputs.js
@@ -1,4 +1,5 @@
 module.exports = {
+  //Enough for one serving
   oneServing: {
     recipe: {
       bread: 10,
@@ -10,5 +11,66 @@ module.exports = {
       peanutButter: 10,
       jam: 5
     }
-  }
+  },
+
+  multipleServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 3875,
+      peanutButter: 1284,
+      jam: 980
+    }
+  },
+
+  //Missing one of the ingredients
+  missingKey: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      peanutButter: 10,
+      jam: 5
+    }
+  },
+
+  zeroQuantity: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 0,
+      jam: 5
+    }
+  },
+
+  //Invalid parameters
+  missingRecipe: {
+    inventory: {
+      bread: 20,
+      peanutButter: 0,
+      jam: 5
+    }
+  },
+
+  missingInventory: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    }
+  },
+
+  missingBoth: {},
+
+  invalidType: 'This is not an object'
+
 }
--- nodejs/test.js
+++ nodejs/test.js
@@ -1,12 +1,58 @@
 const assert = require('assert');
 const {howManyServings} = require('./index');
-const inputs = require('./inputs')
+const inputs = require('./inputs');
 
 describe('Nomnomthrough', () => {
+  //Enough ingredients for 1 serving
   describe('When there is enough ingredient for one serving', () => {
     it('should return 1', () => {
       const output = howManyServings(inputs.oneServing);
       assert.equal(output, 1);
     });
   });
+
+  //Enough ingredients for multiple servings
+  describe('When there is enough ingredients for multiple servings', () => {
+    it('should return 196', () => {
+      const output = howManyServings(inputs.multipleServings);
+      assert.equal(output, 1);
+    });
+  });
+
+  //Missing ingredients
+  describe('When there are ingredients missing', () => {
+    it('should return 0 when an ingredient key is missing', () => {
+      const output = howManyServings(inputs.missingKey);
+      assert.equal(output, 0);
+    });
+
+    it('should also return 0 when ingredient value is zero', () => {
+      const output = howManyServings(inputs.zeroQuantity);
+      assert.equal(output, 0);
+    });
+  });
+
+  //Invalid params
+  describe('When invalid params are passed', () => {
+    it('should return 0 when recipe key is missing', () => {
+      const output = howManyServings(inputs.missingRecipe);
+      assert.equal(output, 0);
+    });
+
+    it('should return 0 when inventory key is missing', () => {
+      const output = howManyServings(inputs.missingInventory);
+      assert.equal(output, 0);
+    });
+
+    it('should return 0 when both recipe and inventory keys are missing', () => {
+      const output = howManyServings(inputs.missingBoth);
+      assert.equal(output, 0);
+    });
+
+    it('should return 0 when params is not an object', () => {
+      const output = howManyServings(inputs.invalidType);
+      assert.equal(output, 0);
+    });
+  });
+
 });
