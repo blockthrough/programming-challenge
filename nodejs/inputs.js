f7d4ec952f4e83facd0da7330ad40bb502183536
Mandeep Singh Gill <mandeepgill.iitkgp@gmail.com>
2018-08-12 20:29:36 +0530

    added test for 10 servings, no serving and extra ingredients in inv

--- nodejs/inputs.js
+++ nodejs/inputs.js
@@ -10,5 +10,55 @@ module.exports = {
       peanutButter: 10,
       jam: 5
     }
+  },
+  zeroServing: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 1
+    }
+  },
+  tenServings: {
+    recipe: {
+      bread: 1,
+      peanutButter: 1,
+      jam: 1
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 15
+    }
+  },
+  extraInventory: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 5,
+      ham: 5
+    }
+  },
+  lackingInventory: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      ham: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 5
+    }
   }
 }
--- nodejs/test.js
+++ nodejs/test.js
@@ -9,4 +9,22 @@ describe('Nomnomthrough', () => {
       assert.equal(output, 1);
     });
   });
+  describe('When there is not enough ingredient even for one serving', () => {
+    it('should return 0', () => {
+      const output = howManyServings(inputs.zeroServing);
+      assert.equal(output, 0);
+    });
+  });
+  describe('When there are enough ingredients for ten servings', () => {
+    it('should return 10', () => {
+      const output = howManyServings(inputs.tenServings);
+      assert.equal(output, 10);
+    });
+  });
+  describe('When there are extra ingredients in inventory', () => {
+    it('should return 1', () => {
+      const output = howManyServings(inputs.extraInventory);
+      assert.equal(output, 1);
+    });
+  });
 });
