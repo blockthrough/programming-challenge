dd0a8de8bbde5516514029da2a92b4e2be91bac9
Mandeep Singh Gill <mandeepgill.iitkgp@gmail.com>
2018-08-12 19:53:42 +0530

    added basic logic

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,30 @@
 function howManyServings (params) {
   const {recipe, inventory} = params
+  let servings = Number.MAX_VALUE;
 
-  // Write code to solve the problem here.
+  for(let ingredient in recipe){
 
-  return 0
+    let quantity = recipe[ingredient];
+
+    // If quantity of ingredient required for recipe is zero, essentialy skip
+    if(quantity === 0){
+      continue;
+    }
+
+    // If recipe ingredient is not present in inventory, nothing can be served
+    if(!(ingredient in inventory) || inventory[ingredient] === 0){
+      return 0;
+    }
+
+    // Core logic to calculate servings size for each ingredient and
+    // taking the minimum of all, with assumption that servings can't
+    // be partial
+    let ingredient_serving = Math.floor(inventory[ingredient]/quantity);
+    servings = Math.min(servings, ingredient_serving);
+
+  }
+
+  return servings;
 }
 
 module.exports = {howManyServings}
