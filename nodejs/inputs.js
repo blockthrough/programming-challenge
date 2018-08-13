8594d127abd552cc16f5053f857b97cdb96b851f
Mike Pollard <pollardm@gmail.com>
2018-08-13 13:38:04 -0400

    Setup tests
    Created serving, no serving, no ingredient and negative ingredient tests

--- nodejs/inputs.js
+++ nodejs/inputs.js
@@ -10,5 +10,137 @@ module.exports = {
       peanutButter: 10,
       jam: 5
     }
+  },
+
+  oneServingWithOptional: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 5,
+      banana: 5
+    }
+  },
+
+  twoServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 20,
+      peanutButter: 10,
+      jam: 10
+    }
+  },
+
+  eightServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 80,
+      peanutButter: 40,
+      jam: 40
+    }
+  },
+
+  noServings: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 9,
+      peanutButter: 4,
+      jam: 4
+    }
+  },
+
+  missingIngredient: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 10,
+      peanutButter: 5
+    }
+  },
+
+  missingIngredientWithOptional: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 10,
+      peanutButter: 5,
+      banana: 5
+    }
+  },
+
+  oneServingEmptyIngredient: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5,
+      banana: 0
+    },
+    inventory: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    }
+  },
+
+  noServingsEmptyIngredient: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5,
+      banana: 0
+    },
+    inventory: {
+      bread: 5,
+      peanutButter: 5,
+      jam: 5
+    }
+  },
+
+  negativeRecipeIngredient: {
+    recipe: {
+      bread: -10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 10,
+      peanutButter: 10,
+      jam: 5
+    }
+  },
+
+  negativeInventoryIngredient: {
+    recipe: {
+      bread: 10,
+      peanutButter: 5,
+      jam: 5
+    },
+    inventory: {
+      bread: 10,
+      peanutButter: 10,
+      jam: -5
+    }
   }
 }
--- nodejs/test.js
+++ nodejs/test.js
@@ -3,10 +3,65 @@ const {howManyServings} = require('./index');
 const inputs = require('./inputs')
 
 describe('Nomnomthrough', () => {
-  describe('When there is enough ingredient for one serving', () => {
+  describe('When there are enough ingredients', () => {
     it('should return 1', () => {
       const output = howManyServings(inputs.oneServing);
       assert.equal(output, 1);
+    });
+
+    it('added an optional ingredient should return 1', () => {
+      const output = howManyServings(inputs.oneServingWithOptional);
+      assert.equal(output, 1);
+    });
+
+    it('should return 2', () => {
+      const output = howManyServings(inputs.twoServings);
+      assert.equal(output, 2);
+    });
+
+    it('should return 8', () => {
+      const output = howManyServings(inputs.eightServings);
+      assert.equal(output, 8);
+    });
+  });
+
+  describe('When there are not enough ingredients for a serving', () => {
+    it('should return 0', () => {
+      const output = howManyServings(inputs.noServings);
+      assert.equal(output, 0);
+    });
+
+    it('missing an ingredient should return 0', () => {
+      const output = howManyServings(inputs.missingIngredient);
+      assert.equal(output, 0);
+    });
+
+    it('switched an optional ingredient should return 0', () => {
+      const output = howManyServings(inputs.missingIngredientWithOptional);
+      assert.equal(output, 0);
+    });
+  });
+
+  describe('When recipe the has an ingredient of 0', () => {
+    it('1 serving - should return 1', () => {
+      const output = howManyServings(inputs.oneServingEmptyIngredient);
+      assert.equal(output, 1);
+    });
+
+    it('0 servings - should return 0', () => {
+      const output = howManyServings(inputs.noServingsEmptyIngredient);
+      assert.equal(output, 0);
+    });
+  });
+
+  describe('When the ingredient is less than 0', () => {
+    it('Recipe - should return 1', () => {
+      const output = howManyServings(inputs.negativeRecipeIngredient);
+      assert.equal(output, 1);
+    });
+    it('Inventory - should return 0', () => {
+      const output = howManyServings(inputs.negativeInventoryIngredient);
+      assert.equal(output, 0);
     });
   });
 });
