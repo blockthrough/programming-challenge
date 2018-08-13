cf441a780b4cd93724f9429e9311dc066c94f674
Mike Pollard <pollardm@gmail.com>
2018-08-13 13:28:24 -0400

    Calculate the number of servings

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,16 @@
-function howManyServings (params) {
-  const {recipe, inventory} = params
+function howManyServings(params) {
+  const { recipe, inventory } = params;
+  let servings = [];
 
-  // Write code to solve the problem here.
 
-  return 0
+  for (let ingredient in recipe) {
+    // Determine the ratio or amount of servings per ingredient
+    const itemServing = Math.floor(inventory[ingredient] / recipe[ingredient]);
+    servings.push(itemServing);
+  }
+
+  // Number of servings will be determined by the lowest number passed in the array
+  return Math.min(...servings);
 }
 
-module.exports = {howManyServings}
+module.exports = { howManyServings }
