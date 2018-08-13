5aea1a2d3c573c25bdd695e798c15113555d9633
Mike Pollard <pollardm@gmail.com>
2018-08-13 13:31:38 -0400

    Add simple checks
    See if ingredient, recipe or inventory exists and has a number greater than 0

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,8 +2,20 @@ function howManyServings(params) {
   const { recipe, inventory } = params;
   let servings = [];
 
+  if (!recipe || !inventory) {
+    throw new Error('Recipe or Inventory object is expected');
+  }
 
   for (let ingredient in recipe) {
+    // If the ingredient in the recipe is less than or greater than 0, we can skip
+    if (recipe[ingredient] <= 0) {
+      continue;
+    }
+    // Determine if there is a valid ingredient in the inventory
+    if (!inventory[ingredient] || inventory[ingredient] <= 0) {
+      return 0;
+    }
+
     // Determine the ratio or amount of servings per ingredient
     const itemServing = Math.floor(inventory[ingredient] / recipe[ingredient]);
     servings.push(itemServing);
