73a3d5acae8036372cd0ebe9542fb86f0d008865
gg <g.p.gianacopoulos@gmail.com>
2018-08-24 17:53:05 -0700

    Implemented challenge code & added tests.

--- nodejs/test.js
+++ nodejs/test.js
@@ -10,3 +10,57 @@ describe('Nomnomthrough', () => {
     });
   });
 });
+
+describe('Nomnomthrough', () => {
+  describe('When there not is enough ingredient for a serving', () => {
+    it('should return 0', () => {
+      const output = howManyServings(inputs.noServings);
+      assert.equal(output, 0);
+    });
+  });
+});
+
+describe('Nomnomthrough', () => {
+  describe('When there is enough ingredient for thirty servings', () => {
+    it('should return 30', () => {
+      const output = howManyServings(inputs.thirtyServings);
+      assert.equal(output, 30);
+    });
+  });
+});
+
+describe('Nomnomthrough', () => {
+  describe('When there is a missing ingredient', () => {
+    it('should return 0', () => {
+      const output = howManyServings(inputs.missingIngredient);
+      assert.equal(output, 0);
+    });
+  });
+});
+
+describe('Nomnomthrough', () => {
+  describe('When there is enough ingredient for one serving, and extra inventory', () => {
+    it('should return 1', () => {
+      const output = howManyServings(inputs.extraIngredient);
+      assert.equal(output, 1);
+    });
+  });
+});
+
+describe('Nomnomthrough', () => {
+  describe('When there is an invalid quantity', () => {
+    it('should return -1', () => {
+      const output = howManyServings(inputs.invalidVal);
+      assert.equal(output, -1);
+    });
+  });
+});
+
+describe('Nomnomthrough', () => {
+  describe('When there is invalid input', () => {
+    it('should return -1', () => {
+      const output = howManyServings(inputs.invalidParams);
+      assert.equal(output, -1);
+    });
+  });
+});
